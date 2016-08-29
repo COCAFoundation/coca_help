1f3725cfa637df7d639e6a89ea9c3041095cae24
David Larrimore <davidlarrimore@gmail.com>
2016-08-29 13:32:04 -0400

    testing redirect fix

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ $app->get('/', function ($request, $response, $args) {
 });
 
 $app->post('/', function ($request, $response, $args) {
-  $app->response->redirect('/', 303);
+  $app->response->withRedirect('/');
 });
 
 
