1f7abf67165412d8e17461ea0c010bd2aa7f0b7f
David Larrimore <davidlarrimore@gmail.com>
2016-08-30 11:39:25 -0400

    Leveraging Slim Config
    Config file injected into SLIM App to allow access

--- index.php
+++ index.php
@@ -10,12 +10,6 @@ require 'vendor/autoload.php';
 
 date_default_timezone_set('America/New_York');
 
-
-/**************
-* CONFIG FILE *
-***************/
-$config_array = parse_ini_file("config/config.ini", true);
-
 /*******************
 * PHP Mailer Setup *
 *******************/
@@ -36,9 +30,20 @@ $mail->Port = 587;                                    // TCP port to connect to
 $mail->isHTML(true);                                  // Set email format to HTML
 */
 
+
+/**************
+* CONFIG FILE *
+**************/
+$config_array = parse_ini_file("config/config.ini", true);
+
+/****************
+* SLIM APP FILE *
+****************/
 $app = new \Slim\App(array(
-    'mode' => $config_array['app']['mode']
+    'mode' => $config_array['app']['mode'],
+    'settings' => $config_array
 ));
+
 
 // Get container
 $container = $app->getContainer();
@@ -50,26 +55,24 @@ $container['view'] = function ($container) {
 
 // monolog
 $container['logger'] = function ($c) {
-    $config_array = parse_ini_file("config/config.ini", true);
     $settings = $c->get('settings');
-    $logger = new Monolog\Logger($config_array['app']['name']);
-    //$logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler("./logs/".$config_array['app']['name'].".log", Monolog\Logger::DEBUG));
+    $logger = new Monolog\Logger($settings['app']['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler("./logs/".$settings['app']['name'].".log", Monolog\Logger::DEBUG));
     $logger->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG)); // <<< uses a stream
 
     return $logger;
 };
 
-
-
+/*****************
+*     ROUTES     *
+*****************/
 $app->get('/', function ($request, $response, $args) {
 
-  $config_array = parse_ini_file("config/config.ini", true);
-
-  $campaignStartDateString = strtotime($config_array['campaign']['campaign_start_date']);
+  $campaignStartDateString = strtotime($this->get('settings')['campaign']['campaign_start_date']);
   $campaignStartDate = date('Y-m-d',$campaignStartDateString);
 
-  $campaignEndDateString = strtotime($config_array['campaign']['campaign_end_date']);
+  $campaignEndDateString = strtotime($this->get('settings')['campaign']['campaign_end_date']);
   $campaignEndDate = date('Y-m-d',$campaignEndDateString);
 
   $todaysDate = date('Y-m-d');
@@ -98,9 +101,12 @@ $app->get('/', function ($request, $response, $args) {
   $this->logger->addInfo("View: ".$theView);
   return $this->view->render($response, $theView, $data);
 
-
 });
 
+
+/***********************
+* ADDED FOR FB SUPPORT *
+***********************/
 $app->post('/', function ($request, $response, $args) {
   return $this->response->withRedirect('/');
 });
